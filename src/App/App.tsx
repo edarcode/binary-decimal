72ef7893c9f9812c046b6f7cd4549fbb94243b7e
Edwin Ortiz <edarcode@gmail.com>
2024-08-22 14:02:45 -0500

    mvp funcional

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,50 @@
-import "./App.css";
+import { useState } from "react";
+import { FormEvent } from "../types";
+import css from "./css.module.css";
 
-function App() {
-  return <div>¡Hola mundo!</div>;
+export default function App() {
+	const [result, setResult] = useState("");
+
+	const convert = (e: FormEvent) => {
+		e.preventDefault();
+		const form = new FormData(e.currentTarget);
+		const baseOrigin = Number(form.get("base-origin"));
+		const baseToConvert = Number(form.get("base-to-convert"));
+		const num = form.get("num") as string;
+
+		const parse = parseInt(num, baseOrigin);
+		const result = parse.toString(baseToConvert);
+		setResult(result);
+	};
+
+	return (
+		<div className={css.app}>
+			<form onSubmit={convert}>
+				<select name="base-origin">
+					<option value={BASE.decimal}>Decimal</option>
+					<option value={BASE.binario}>Binario</option>
+					<option value={BASE.optal}>Optal</option>
+					<option value={BASE.hexadecimal}>Hexadecimal</option>
+				</select>
+
+				<select name="base-to-convert">
+					<option value={BASE.decimal}>Decimal</option>
+					<option value={BASE.binario}>Binario</option>
+					<option value={BASE.optal}>Optal</option>
+					<option value={BASE.hexadecimal}>Hexadecimal</option>
+				</select>
+
+				<input type="number" name="num" />
+				<button>Convertir</button>
+			</form>
+			<div>{result}</div>
+		</div>
+	);
 }
 
-export default App;
+const BASE = {
+	binario: 2,
+	decimal: 10,
+	optal: 8,
+	hexadecimal: 16
+};
